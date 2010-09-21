b4af571c602ccc3526c2355ecea0771298c05c8c
Matt Kirman <matt@mattkirman.com>
2010-09-21 11:25:06 +0100

    Started Citrus

---
+++ app/models/test.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Models;
+class Test extends \Citrus\Model
+{
+
+    
+
+}
---
+++ scarlet/citrus/model.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Requires PHP 5.3
+ *
+ * Scarlet : An event driven PHP framework.
+ * Copyright (c) 2010, Matt Kirman <matt@mattkirman.com>
+ *
+ * Licensed under the GPL license
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright 2010, Matt Kirman <matt@mattkirman.com>
+ * @package scarlet
+ * @subpackage citrus
+ * @license GPLv2 <http://www.gnu.org/licenses/gpl-2.0.html>
+ */
+namespace Citrus;
+/**
+ * The core Model class. This is the class that all Citrus models in your
+ * application should inherit from.
+ */
+class Model
+{
+
+    
+
+}
